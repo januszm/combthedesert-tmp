017ccde8c08909a2c7da4d663e4cc1a373501258
Janusz Mordarski <janusz.m@gmail.com>
2017-01-10 00:13:38 +0100

    Handle some page open errors

--- app/services/scrape_page_url.rb
+++ app/services/scrape_page_url.rb
@@ -9,14 +9,24 @@ class ScrapePageUrl
   end
 
   def call
-    html = open(url)
-    # TODO handle errors
+    html = open_url
+    return html if html.is_a?(Hash) && html.has_key?(:error)
     doc = Nokogiri::HTML(html)
     %i(h1 h2 h3).each do |header|
       doc.css(header).each { |h| @content[header] << h.text }
     end
     doc.css('a').each { |a| @content[:links] << a['href'] }
     @content
+  end
+
+  private
+
+  def open_url
+    html = open(@url)
+  rescue OpenURI::HTTPError => error
+    return { error: error.io.status }
+  rescue Exception => error
+    return { error: [error.message] }
   end
 
 end
--- test/services/scrape_page_url_test.rb
+++ test/services/scrape_page_url_test.rb
@@ -2,12 +2,18 @@ require 'test_helper'
 
 class ScrapePageUrlTest < ActiveSupport::TestCase
 
-  test 'scrape content from url' do
+  test 'scrape valid content from url' do
     url = 'http://example.com'
     content =  ScrapePageUrl.new(url).call
     assert_equal content[:url], 'http://example.com'
     assert_equal content[:h1], ['Example Domain']
     assert_equal content[:links], ['http://www.iana.org/domains/example']
+  end
+
+  test 'scrape invalid content from url' do
+    url = 'http://nowhere.com'
+    content =  ScrapePageUrl.new(url).call
+    assert content.has_key?(:error)
   end
 
 end
