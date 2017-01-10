c437c83a252c18480def24d3801a9e8d05029861
Janusz Mordarski <janusz.m@gmail.com>
2017-01-10 11:06:29 +0100

    Add versions table for PaperTrail

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109125747) do
+ActiveRecord::Schema.define(version: 20170110100516) do
 
   create_table "pages", force: :cascade do |t|
     t.string   "url"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",                     null: false
+    t.integer  "item_id",                       null: false
+    t.string   "event",                         null: false
+    t.string   "whodunnit"
+    t.text     "object",     limit: 1073741823
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
 end
