b01878d09a13883a442cde13cfedc6064c925ecf
Janusz Mordarski <janusz.m@gmail.com>
2017-01-10 12:32:51 +0100

    Update page records identified by normalized URL
    This way we can actually enable version tracking.

--- app/jobs/scrape_page_job.rb
+++ app/jobs/scrape_page_job.rb
@@ -2,7 +2,9 @@ class ScrapePageJob < ApplicationJob
   queue_as :default
 
   def perform(url)
+    url = url.gsub(/(\/)+$/,'')
     content = ScrapePageUrl.new(url).call
-    Page.create(url: url, content: content)
+    page = Page.find_or_create_by(url: url)
+    page.update(content: content)
   end
 end
